0057332530dd8962f24aa9ddc6d1b40301c355ca
Maksym Petriv <petriv050711@gmail.com>
2025-06-20 14:55:47 +0200

    Services modif as Monacair

--- components/management/maintenance.tsx
+++ components/management/maintenance.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import { useTranslations } from 'next-intl'
 import { Wrench, MapPin, Building, Landmark, Ship, Castle, Mountain } from 'lucide-react'
 import { BentoGrid, BentoGridItem } from '../ui/bento-grid'
+import { Button } from 'components/ui/button'
+import { Link } from '@/i18n/navigation'
 
 export default function Maintenance() {
   const t = useTranslations('Management.maintenance')
@@ -34,6 +36,15 @@ export default function Maintenance() {
     <section className="py-16 bg-white">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row items-center gap-12">
+          <div className="md:w-1/2 w-full h-[300px] md:h-[500px] relative mb-8 md:mb-0">
+            <Image
+              src="/images/index/hero.webp"
+              alt="Maintenance d'hélicoptères"
+              fill
+              sizes="(max-width: 768px) 100vw, 50vw"
+              className="object-cover rounded-lg shadow-lg"
+            />
+          </div>
           <div className="md:w-1/2">
             <div className="flex items-center mb-6">
               <div className="w-12 h-12 rounded-full bg-primary-button/10 flex items-center justify-center mr-4">
@@ -49,7 +60,7 @@ export default function Maintenance() {
             </div>
 
             <div className="mt-8">
-              <BentoGrid className="grid-cols-2 md:grid-cols-3 gap-3 md:auto-rows-[6rem]">
+              <BentoGrid className="grid-cols-2 md:grid-cols-3 gap-3 py-3 md:auto-rows-[6rem]">
                 {locations.map((location, index) => (
                   <BentoGridItem
                     key={index}
@@ -71,17 +82,10 @@ export default function Maintenance() {
                   />
                 ))}
               </BentoGrid>
+              <Link href={'/services/maintenance'}>
+                <Button>{t('CTA')}</Button>
+              </Link>
             </div>
-          </div>
-
-          <div className="md:w-1/2 w-full h-[300px] md:h-[500px] relative mb-8 md:mb-0">
-            <Image
-              src="/images/index/hero.webp"
-              alt="Maintenance d'hélicoptères"
-              fill
-              sizes="(max-width: 768px) 100vw, 50vw"
-              className="object-cover rounded-lg shadow-lg"
-            />
           </div>
         </div>
       </div>

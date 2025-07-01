8144ca7f387b5d544085ca30ca1262234f2b6f2a
Gregory Buffard <gregory442005@gmail.com>
2025-07-01 12:36:22 +0200

    Updates About page
    Adds and modifies text

--- components/about-us/monacair-leader.tsx
+++ components/about-us/monacair-leader.tsx
@@ -5,17 +5,17 @@ export default async function MonacairLeader() {
 
   return (
     <section className="py-16 bg-gray-50">
-      <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-gray-800 mb-8 font-brother">{t('title')}</h2>
-
-        <div className="space-y-6 text-gray-600 font-brother">
-          <h4 className={'text-xl'}>{t('operational-bases')}</h4>
-          <ul>
-            <li>{t('team-goal')}</li>
-            <li>{t('services')}</li>
-            <li>{t('maintenance')}</li>
-          </ul>
-        </div>
+      <div className="container mx-auto px-4 space-y-8">
+        {Array.from({ length: 2 }).map((_, i) => (
+          <div key={i} className={'space-y-4'}>
+            <h2 className="text-3xl font-bold text-gray-800 mb-8 font-brother">{t(`title${i}`)}</h2>
+            <p className={'text-gray-600 font-brother'}>
+              {t.rich(`text${i}`, {
+                br: (chunks) => <br />,
+              })}
+            </p>
+          </div>
+        ))}
       </div>
     </section>
   )
--- components/about-us/our-history.tsx
+++ components/about-us/our-history.tsx
@@ -9,19 +9,25 @@ export default async function OurHistory() {
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row items-center gap-12">
           <div className="md:w-1/2">
-            <p className="text-gray-600 mb-6 text-lg font-brother">{t('foundation')}</p>
-            <p className="text-gray-600 mb-6 text-xl font-bold font-brother">
-              {t('services-intro')}
+            <p className="text-gray-600 mb-6 text-lg font-brother">
+              {t.rich('foundation', {
+                b: (chunks) => <b>{chunks}</b>,
+              })}
             </p>
-            <h4 className={'text-gray-600 mb-6 text-xl font-extrabold font-brother'}>
-              {t('services-list.ateliers')}
-            </h4>
+            <p className="text-gray-600 mb-6 text-lg font-brother">
+              {t.rich('services-intro', {
+                b: (chunks) => <b>{chunks}</b>,
+              })}
+            </p>
             <ul className="list-disc pl-6 mb-6 text-gray-600 space-y-2 font-brother">
-              <li>{t('services-list.visite')}</li>
-              <li>{t('services-list.management')}</li>
-              <li>{t('services-list.charter')}</li>
+              {Array.from({ length: 7 }, (_, i) => (
+                <li key={i}>
+                  {t.rich(`services-list.item${i}`, {
+                    b: (chunks) => <b>{chunks}</b>,
+                  })}
+                </li>
+              ))}
             </ul>
-            <p className="text-gray-600 mb-6 font-brother">{t('official-provider')}</p>
           </div>
           <div className="md:w-1/2 w-full h-[300px] md:h-[400px] relative mt-6 md:mt-0">
             <Image

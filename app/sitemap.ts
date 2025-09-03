595bfc22cfd13fdf2ab9e0bf212a1d83c9a34923
Maksym Petriv <petriv050711@gmail.com>
2025-09-03 22:56:38 +0200

    New sitemap for better indexing

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,192 +3,146 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://helisecurite.fr',
+      url: 'https://helisecurite.fr/fr',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en',
-          fr: 'https://helisecurite.fr/fr',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/about',
+      url: 'https://helisecurite.fr/fr/about',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/about',
-          fr: 'https://helisecurite.fr/fr/about',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/contact',
+      url: 'https://helisecurite.fr/fr/contact',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/contact',
-          fr: 'https://helisecurite.fr/fr/contact',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/destinations',
+      url: 'https://helisecurite.fr/fr/destinations',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/destinations',
-          fr: 'https://helisecurite.fr/fr/destinations',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/events',
+      url: 'https://helisecurite.fr/fr/events',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/events',
-          fr: 'https://helisecurite.fr/fr/events',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/experiences',
+      url: 'https://helisecurite.fr/fr/experiences',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/experiences',
-          fr: 'https://helisecurite.fr/fr/experiences',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/fleet',
+      url: 'https://helisecurite.fr/fr/fleet',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/fleet',
-          fr: 'https://helisecurite.fr/fr/fleet',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/private-jet',
+      url: 'https://helisecurite.fr/fr/private-jet',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/private-jet',
-          fr: 'https://helisecurite.fr/fr/private-jet',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/services',
+      url: 'https://helisecurite.fr/fr/services',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/services',
-          fr: 'https://helisecurite.fr/fr/services',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/services/handling',
+      url: 'https://helisecurite.fr/fr/services/handling',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/services/handling',
-          fr: 'https://helisecurite.fr/fr/services/handling',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/services/maintenance',
+      url: 'https://helisecurite.fr/fr/services/maintenance',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/services/maintenance',
-          fr: 'https://helisecurite.fr/fr/services/maintenance',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/services/management',
+      url: 'https://helisecurite.fr/fr/services/management',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/services/management',
-          fr: 'https://helisecurite.fr/fr/services/management',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/legal',
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          en: 'https://helisecurite.fr/en/legal',
-          fr: 'https://helisecurite.fr/fr/legal',
-        },
-      },
-    },
-    {
-      url: 'https://helisecurite.fr/legal/confidentiality',
+      url: 'https://helisecurite.fr/fr/legal/confidentiality',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/legal/confidentiality',
-          fr: 'https://helisecurite.fr/fr/legal/confidentiality',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/legal/cookies',
+      url: 'https://helisecurite.fr/fr/legal/cookies',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/legal/cookies',
-          fr: 'https://helisecurite.fr/fr/legal/cookies',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/legal/mentions',
+      url: 'https://helisecurite.fr/fr/legal/mentions',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/legal/mentions',
-          fr: 'https://helisecurite.fr/fr/legal/mentions',
         },
       },
     },
     {
-      url: 'https://helisecurite.fr/flights',
+      url: 'https://helisecurite.fr/fr/flights',
       lastModified: new Date(),
       alternates: {
         languages: {
           en: 'https://helisecurite.fr/en/flights',
-          fr: 'https://helisecurite.fr/fr/flights',
-        },
-      },
-    },
-    {
-      url: 'https://helisecurite.fr/flights/regular/nice/monaco',
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          en: 'https://helisecurite.fr/en/flights/regular/nice/monaco',
-          fr: 'https://helisecurite.fr/fr/flights/regular/nice/monaco',
-        },
-      },
-    },
-    {
-      url: 'https://helisecurite.fr/flights/regular/monaco/nice',
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          en: 'https://helisecurite.fr/en/flights/regular/monaco/nice',
-          fr: 'https://helisecurite.fr/fr/flights/regular/monaco/nice',
         },
       },
     },
